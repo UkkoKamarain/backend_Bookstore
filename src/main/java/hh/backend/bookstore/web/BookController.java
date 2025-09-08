a85309ee8c58d09b16a9a6c083f8278a0f4d80cf
ukko.kamarainen@myy.haaga-helia.fi <ukko.kamarainen@myy.haaga-helia.fi>
2025-09-08 13:01:28 +0300

    Done tasks specified in c3p2

--- src/main/java/hh/backend/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/web/BookController.java
@@ -1,11 +1,20 @@
 package hh.backend.bookstore.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import hh.backend.bookstore.domain.Book;
+import hh.backend.bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
 
     @GetMapping("/index")
     public String getIndex(
@@ -14,4 +23,12 @@ public class BookController {
         return "welcome"; // welcome.html
     }
 
+    @GetMapping("/booklist")
+    public String getBookList(
+        Model model
+    ) {
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist"; // booklist.html
+    }
 }
