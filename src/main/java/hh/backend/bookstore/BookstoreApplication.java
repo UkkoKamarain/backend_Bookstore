374bed739e6eca3bb266550a5b9570983a849650
ukko.kamarainen@myy.haaga-helia.fi <ukko.kamarainen@myy.haaga-helia.fi>
2025-09-15 11:27:09 +0300

    Done tasks specified in c4p1

--- src/main/java/hh/backend/bookstore/BookstoreApplication.java
+++ src/main/java/hh/backend/bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.backend.bookstore.domain.Book;
 import hh.backend.bookstore.domain.BookRepository;
+import hh.backend.bookstore.domain.Category;
+import hh.backend.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -19,11 +21,21 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookstoreRunner(BookRepository bookRepository) {
+	public CommandLineRunner bookstoreRunner(
+			BookRepository bookRepository, 
+			CategoryRepository categoryRepository
+		) {
 		return (args) -> {
+
+			log.info("Save some sample categories");
+			// adding some categories
+			categoryRepository.save(new Category("fantasy"));
+			categoryRepository.save(new Category("science"));
+			categoryRepository.save(new Category("romance"));
+
 			log.info("Insert testing books");
 
-			// adding a some test books
+			// adding some test books
 			bookRepository.save(new Book(
 					"TestBook1",
 					"TestAuthor1",
@@ -36,6 +48,11 @@ public class BookstoreApplication {
 					2024,
 					"TestIsbn2",
 					300.33));
+
+			log.info("Fetch all categories");
+			for (Category c : categoryRepository.findAll()) {
+				log.info(c.toString());
+			}
 
 			log.info("Fetch all books");
 			for (Book b : bookRepository.findAll()) {
