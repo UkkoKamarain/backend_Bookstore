11d8eae2528004415a35f35fa4d0031368b25124
ukko.kamarainen@myy.haaga-helia.fi <ukko.kamarainen@myy.haaga-helia.fi>
2025-09-22 14:35:25 +0300

    Done tasks specified in c5p2

--- src/main/java/hh/backend/bookstore/web/BookRestController.java
+++ src/main/java/hh/backend/bookstore/web/BookRestController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.backend.bookstore.domain.Book;
@@ -29,5 +31,12 @@ public class BookRestController {
         @PathVariable("id") Long bookId 
     ) {
         return bookRepository.findById(bookId);
+    }
+
+    @PostMapping("/books")
+    public Book saveBookRest(
+        @RequestBody Book book
+    ) {
+        return bookRepository.save(book);
     }
 }
